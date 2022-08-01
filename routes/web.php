c530d8a7658eee09fec816e59726e6fd555eacd4
Sayeed <40250983+sayeed13@users.noreply.github.com>
2022-08-01 20:27:28 +0600

    Display Question
    install laravel ui, fixed paginate broken, question controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('questions', QuestionController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
