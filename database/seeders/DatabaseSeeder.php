db541acd9b6b1ffa56fe11da21883e7129d51875
Sayeed <40250983+sayeed13@users.noreply.github.com>
2022-08-01 18:55:49 +0600

    Question and user factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Question;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(3)->create()
+        //     ->each(function($ques){
+        //         $ques->questions()
+        //             ->saveMany(
+        //                 \App\Models\Question::factory(5)->make()
+        //             );
+        //     });
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        User::factory(3)->has(Question::factory()->count(3))->create();
+
+        
     }
 }
