bac48815b4281b7dae636ef57bbb2ff51d72cccd
Sayeed <40250983+sayeed13@users.noreply.github.com>
2022-08-13 20:18:14 +0600

    Building question Form

---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex justify-content-between">
+                        <h2>Ask Question</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">
+                                All Questions
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <form action="{{ route('questions.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group mb-3">
+                            <label for="question-title">Question Title</label>
+                            <input type="text" name="title" id="question-title" class="form-control @error('title') is-invalid @enderror">
+
+                            @error('title')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="question-body">Question Details</label>
+                            <textarea name="body" id="question-body" rows="10" class="form-control @error('body') is-invalid @enderror"></textarea>
+
+                            @error('body')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group mb-3">
+                            <button type="submit" class="btn btn-outline-primary">Ask this Question</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
